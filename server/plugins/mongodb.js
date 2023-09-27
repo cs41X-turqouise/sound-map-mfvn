5e8c5919f82318f97d2e6bf7f0619eea4f6029ad
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-09-27 00:08:25 -0400

    Update mongodb.js
    - fix connection event, needs to be added before we initiate the connection (duh)
    - fix onCLose event, removed depreciated callback. Changed to async and await the connection closing then log it

--- server/plugins/mongodb.js
+++ server/plugins/mongodb.js
@@ -4,6 +4,16 @@ const fp = require('fastify-plugin')
 const mongoose = require('mongoose')
 
 module.exports = fp(async function (fastify, options) {
+  // Log a message when the MongoDB connection is established
+  mongoose.connection.on('connected', () => {
+    fastify.log.info('MongoDB connection established!');
+  });
+  // Listen for the 'close' event on the Fastify instance and close the Mongoose connection
+  fastify.addHook('onClose', async (instance) => {
+    await mongoose.connection.close();
+    fastify.log.info('MongoDB connection closed.');
+  });
+
   // Connect to the MongoDB database using Mongoose
   await mongoose.connect(fastify.config.MONGODB_URL, {
     useNewUrlParser: true, useUnifiedTopology: true
@@ -11,14 +21,4 @@ module.exports = fp(async function (fastify, options) {
 
   // Decorate the Fastify instance with the Mongoose client
   fastify.decorate('mongoose', mongoose);
-
-  // Log a message when the MongoDB connection is established
-  mongoose.connection.on('connected', () => {
-    console.log('MongoDB connection established!');
-  });
-
-  // Listen for the 'close' event on the Fastify instance and close the Mongoose connection
-  fastify.addHook('onClose', (instance, done) => {
-    mongoose.connection.close(done);
-  });
 });
