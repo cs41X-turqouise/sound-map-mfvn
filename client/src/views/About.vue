e0d15936967dfcbc3d0f9a7ff3844a35305716a7
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-09-27 17:13:03 -0400

    Update About.vue
    - basic vue page setup

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <!-- <iframe
+      src="https://cs41x-turqouise.github.io/team-website/"
+      frameborder="1"
+      style="width: 100vw; height: 90vh;">
+    </iframe> -->
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+};
+</script>
+
+<style scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
