c512f6e4421057a4b940b2d5f46252313883c13d
Cwoody240 <98424636+Cwoody240@users.noreply.github.com>
2023-10-02 16:14:26 -0400

    Update root.js

--- server/routes/root.js
+++ server/routes/root.js
@@ -5,15 +5,3 @@ module.exports = async function (fastify, opts) {
     return { root: 'Hello World' }
   })
 }
-
-const fastify = require('fastify')();
-const uploadRoute = require('./uploads/index.js'); // Adjust the path as needed
-
-fastify.register(uploadRoute);
-
-// Add any other routes or plugins as necessary
-
-fastify.listen(3001, (err) => {
-  if (err) throw err;
-  console.log('Server is running on http://localhost:3001');
-});
