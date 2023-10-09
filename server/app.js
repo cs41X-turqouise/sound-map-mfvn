e6700be135b19312803f30d7c7ba7c6f0c7c8c09
MeagherPatrick <pmeag001@odu.edu>
2023-10-09 13:33:31 -0400

    Added 'oauth' dir to list of routes to auto load

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const AutoLoad = require('@fastify/autoload')
 
+
 /**
  * @typedef {import('fastify').FastifyInstance} FastifyInstance
  */
@@ -65,7 +66,7 @@ module.exports = async function (fastify, opts) {
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
+    dir: path.join(__dirname, 'routes' , 'oauth'),
     options: Object.assign({}, opts)
   })
 }
