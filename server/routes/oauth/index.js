1366d206e4c3708fce82854278099cfd17f6e7a2
MeagherPatrick <pmeag001@odu.edu>
2023-10-09 13:37:33 -0400

    Fixed access token & Changed 'this' to 'fastify'

--- server/routes/oauth/index.js
+++ server/routes/oauth/index.js
@@ -1,27 +1,39 @@
+// server/routes/oauth/index.js
+
 'use strict';  // Use JavaScript in "strict" mode to catch common bugs.
 
 const sget = require('simple-get');  // Require 'simple-get' for making HTTP requests.
-
+console.log('registering Oauth Route...');
 // Define the route handlers for the Google OAuth2 callback.
 module.exports = function (fastify, opts, next) {
+// console.log(fastify.googleOAuth2);
     // Define a GET route for the OAuth2 callback.
     fastify.get('/login/google/callback', function (request, reply) {
+     
+        // console.log(fastify.googleOAuth2);
         // Use the 'getAccessTokenFromAuthorizationCodeFlow' method from the Fastify OAuth2 plugin to obtain an access token.
-        this.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request, (err, result) => {
+        fastify.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request, (err, result) => {
             if (err) {
                 reply.send(err);  // Send the error as a response if there's any.
                 return;
             }
+            console.log("Error:", err);
+console.log("Result:", result);
+
 
             // Make a request to Google's API to get user information.
-            sget.concat({
+            console.log("Access Token:", result.token.access_token);
+            sget.concat({             
+
                 url: 'https://www.googleapis.com/oauth2/v2/userinfo',  // Google's endpoint for user information.
                 method: 'GET',
                 headers: {
-                    Authorization: 'Bearer ' + result.access_token  // Set the Authorization header with the obtained access token.
+                    Authorization: 'Bearer ' + result.token.access_token  // Set the Authorization header with the obtained access token.
                 },
                 json: true  // Expect a JSON response.
-            }, function (err, res, data) {
+            }, 
+            
+            function (err, res, data) {
                 if (err) {
                     reply.send(err);  // Send the error as a response if there's any.
                     return;
