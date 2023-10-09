ac7e660913c665b18374bae914494c05c0bf351a
MeagherPatrick <pmeag001@odu.edu>
2023-10-09 13:36:23 -0400

    Changed to Export plugin explicitly.

--- server/plugins/auth.js
+++ server/plugins/auth.js
@@ -1,11 +1,15 @@
+// server/plugins/auth.js
+
 'use strict';  // Use JavaScript in "strict" mode to catch common bugs.
+const fp = require('fastify-plugin');
 
 // This plugin registers the OAuth2 configuration for Google authentication.
-module.exports = async function (fastify, options) {
+ async function oauth2Plugin(fastify, options) {
     // Register the Fastify OAuth2 plugin.
     fastify.register(
         require('@fastify/oauth2'),  // Require the Fastify OAuth2 plugin.
         {
+           
             name: 'googleOAuth2',   // Name of the authentication strategy.
             scope: ['profile'],     // The scope for the OAuth2 request. 'profile' gives access to basic profile information.
             credentials: {
@@ -18,5 +22,8 @@ module.exports = async function (fastify, options) {
             startRedirectPath: '/login/google',                      // Path to redirect user for authentication.
             callbackUri: 'http://localhost:3000/login/google/callback'  // The callback URL Google will redirect to after authentication.
         }
-    );
+        
+        );
+        //  console.log(fastify.googleOAuth2);
 }
+module.exports = fp(oauth2Plugin);
