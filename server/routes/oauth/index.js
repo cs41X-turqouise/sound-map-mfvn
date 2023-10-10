5922a4198185e4e4841876f0d0aab71946fc68ff
MeagherPatrick <pmeag001@odu.edu>
2023-10-10 15:53:28 -0400

    removed Debugging and added redirect with user data

--- server/routes/oauth/index.js
+++ server/routes/oauth/index.js
@@ -6,25 +6,21 @@ const sget = require('simple-get');  // Require 'simple-get' for making HTTP req
 console.log('registering Oauth Route...');
 // Define the route handlers for the Google OAuth2 callback.
 module.exports = function (fastify, opts, next) {
-// console.log(fastify.googleOAuth2);
+// console.log("fastify.googleOAuth2");
     // Define a GET route for the OAuth2 callback.
     fastify.get('/login/google/callback', function (request, reply) {
      
-        // console.log(fastify.googleOAuth2);
         // Use the 'getAccessTokenFromAuthorizationCodeFlow' method from the Fastify OAuth2 plugin to obtain an access token.
         fastify.googleOAuth2.getAccessTokenFromAuthorizationCodeFlow(request, (err, result) => {
             if (err) {
                 reply.send(err);  // Send the error as a response if there's any.
                 return;
             }
-            console.log("Error:", err);
-console.log("Result:", result);
-
-
             // Make a request to Google's API to get user information.
-            console.log("Access Token:", result.token.access_token);
-            sget.concat({             
-
+            // console.log("Access Token:", result.token.access_token);
+            sget.concat({      
+                
+                
                 url: 'https://www.googleapis.com/oauth2/v2/userinfo',  // Google's endpoint for user information.
                 method: 'GET',
                 headers: {
@@ -34,11 +30,15 @@ console.log("Result:", result);
             }, 
             
             function (err, res, data) {
+                
                 if (err) {
                     reply.send(err);  // Send the error as a response if there's any.
                     return;
                 }
-                reply.send(data);  // Send the user data as a response.
+                const redirectUrl = `http://localhost:5173/oauth/callback?name=${data.name}&id=${data.id}&picture=${data.picture}`;
+                reply.redirect(redirectUrl); // Send the user data as a response.
+                console.log('fastasdfkjashdfkjasdhfk\nasdasdsdasify.googleOAuth2');
+
             });
         });
     });
