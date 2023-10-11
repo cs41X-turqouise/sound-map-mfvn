7b023c444cd3567f73c0294a8e3d41845a67a26b
Aamr Ibrahim <iaamr6646@gmail.com>
2023-10-10 23:51:15 -0400

    about page commit

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,61 +1,67 @@
 <template>
-  <div class="about">
-    <iframe
-      src="https://cs41x-turqouise.github.io/team-website/"
-      frameborder="1"
-      style="width: 100vw; height: 90vh;">
-    </iframe>
-    <section class="project-description">
-      <h2>Project Overview</h2>
-      <p>
-        The Sound Map Prototype is an innovative project that combines the beauty of maps with the magic of sound. Our mission is to create an immersive experience that allows users to explore the world through soundscapes.
-      </p>
-    </section>
+  <div class="about-page">
+      <body>
+      <i><h1>About Sound Map For Changing Landscape</h1></i>
 
-    <section class="goals">
-      <h2>Our Goals</h2>
-      <ul>
-        <li>Create a captivating and educational tool for exploring the world's acoustic diversity.</li>
-        <li>Empower users to discover unique sounds and their geographical origins.</li>
-        <li>Highlight the importance of preserving acoustic environments and biodiversity.</li>
-      </ul>
-    </section>
-
-    <section class="how-it-works">
-      <h2>How It Works</h2>
-      <p>
-        Our sound map prototype is designed to provide an interactive and user-friendly interface. Users can navigate a map, click on markers, and listen to sound recordings from various locations around the globe. Each sound is accompanied by information about its source, including its cultural and ecological significance.
+      <h2>Welcome and Introduction: </h2>
+      <p>Welcome to Sound Map For Changing Landscape about page, where we are happy to present our creative solution aimed
+        at focus on the critical issue of climate change in the Tidewater Region.</p>
+      <h2>Description of the Interactive Sound Map: </h2>
+      <p>The Interactive Sound Map is a critical tool for tackling global sea-level rise, with a particular emphasis on
+        the
+        Chesapeake Bay region. It provides a dynamic awareness of the shifting aural landscapes as sea levels rise. Users
+        can listen to and upload sound recordings, enabling community involvement among people affected and worried by
+        environmental changes.
       </p>
-    </section>
+      <h2>Problem Background</h2>
+      <p>In the Chesapeake Bay region, rising sea levels present a range of critical issues, including erosion, flooding,
+        saltwater contamination, and habitat destruction. These interconnected challenges demand a coordinated and timely
+        response to protect both the environment and local communities.</p>
 
-    <section class="team">
-      <h2>Meet the Team</h2>      
-      <p>
-        We are a passionate team of developers, sound enthusiasts, and environmentalists dedicated to bringing this project to life. Our diverse backgrounds and shared commitment to environmental awareness drive our efforts to make the world's sonic tapestry accessible to all.
-      </p>
-    </section>
+      <h2>Problem Characteristics: </h2>
+      <p>Our Interactive Sound Map project combines historical records with present user contributions to create a
+        complete
+        perspective of the aural landscapes of the Chesapeake Bay. Historical records provide insights into the past,
+        recording the region's rich history and providing as a crucial point of reference for change. </p>
 
-    <section class="contact">
-      <h2>Contact Us</h2>
+      <h2>Solution Statement & Characteristics: </h2>
+      <p>We have a solution: a special map with sounds that shows how the weather is changing in the Tidewater Region.
+        our map combines old and new audio and video from people like you to help us understand things better.</p>
+
+      <h2>Our Project's Mission: </h2>
       <p>
-        If you have questions, suggestions, or would like to collaborate with us, we'd love to hear from you! Feel free to reach out to us via email or social media.
+        The goal of the project is to raise awareness and build a greater knowledge of the issues faced by increasing sea
+        levels, with a particular emphasis on the Chesapeake Bay region. It strives to involve and empower those who are
+        directly affected by environmental changes, while also acting as a useful resource for policymakers and concerned
+        citizens.
       </p>
-      </section>
+      </body>
     </div>
 </template>
 
-<script>
-export default {
-  name: "About",
-};
-</script>
-
 <style scoped>
-.about {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
+.about-page {
+
+  margin: 0 auto;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+}
+
+h2 {
+  font-size: 20px;
+  margin-top: 20px;
+}
+
+p {
+  font-size: 16px;
+  line-height: 1.5;
+  margin: 10px 0;
 }
 </style>
+
+<!-- 
+<script>
+export default {
+  name: "about",
+};
+</script> -->
