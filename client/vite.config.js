d0b2f647f57069618eae360b7ebc90b132e1425b
MeagherPatrick <pmeag001@odu.edu>
2023-10-13 15:58:06 -0400

    Change/Add: default ports for use with Docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3030,
+  },
+  preview: {
+    port: 8080,
+  },
   plugins: [vue()],
 })
