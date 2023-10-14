1a0161970a921fb4d28991ed6da937a25c3c680a
MeagherPatrick <pmeag001@odu.edu>
2023-10-14 02:20:29 -0400

    Added: Set default host '0.0.0.0' & port' 8080'.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  preview: {
-    port: 8080,
+  server: {
+    host: '0.0.0.0',
+    port: 8080
   },
   plugins: [vue()],
 })
