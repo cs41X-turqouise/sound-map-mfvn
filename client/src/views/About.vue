17e30555a651be135506980da7d517040dd97b7a
Aamr Ibrahim <iaamr6646@gmail.com>
2023-10-16 20:30:09 -0400

    about page updated but not completed

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about-page">
+    <v-toolbar><router-link to="/home" tag="div" class="v-toolbar__title">Home</router-link></v-toolbar>
       <body>
       <i><h1>About Sound Map For Changing Landscape</h1></i>
-
       <h2>Welcome and Introduction: </h2>
       <p>Welcome to Sound Map For Changing Landscape about page, where we are happy to present our creative solution aimed
         at focus on the critical issue of climate change in the Tidewater Region.</p>
@@ -17,24 +17,17 @@
       <p>In the Chesapeake Bay region, rising sea levels present a range of critical issues, including erosion, flooding,
         saltwater contamination, and habitat destruction. These interconnected challenges demand a coordinated and timely
         response to protect both the environment and local communities.</p>
-
+        <hr>
       <h2>Problem Characteristics: </h2>
       <p>Our Interactive Sound Map project combines historical records with present user contributions to create a
         complete
         perspective of the aural landscapes of the Chesapeake Bay. Historical records provide insights into the past,
         recording the region's rich history and providing as a crucial point of reference for change. </p>
-
+        <hr>
       <h2>Solution Statement & Characteristics: </h2>
       <p>We have a solution: a special map with sounds that shows how the weather is changing in the Tidewater Region.
         our map combines old and new audio and video from people like you to help us understand things better.</p>
 
-      <h2>Our Project's Mission: </h2>
-      <p>
-        The goal of the project is to raise awareness and build a greater knowledge of the issues faced by increasing sea
-        levels, with a particular emphasis on the Chesapeake Bay region. It strives to involve and empower those who are
-        directly affected by environmental changes, while also acting as a useful resource for policymakers and concerned
-        citizens.
-      </p>
       </body>
     </div>
 </template>
@@ -43,19 +36,23 @@
 .about-page {
 
   margin: 0 auto;
-  padding: 20px;
+  padding: 1,000px;
   font-family: Arial, sans-serif;
 }
 
 h2 {
   font-size: 20px;
-  margin-top: 20px;
+  margin-top: 10px;
 }
 
 p {
-  font-size: 16px;
+  font-size: 20px;
   line-height: 1.5;
   margin: 10px 0;
+}
+.v-toolbar {
+  height: 40px; /* Adjust the height to make the toolbar smaller */
+  /* Add other styles as needed */
 }
 </style>
 
