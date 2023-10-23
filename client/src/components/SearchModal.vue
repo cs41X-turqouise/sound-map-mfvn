f97bac66e69ab6d40f55a26bd2d10ae51698a968
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-10-23 11:44:23 -0400

    Update SearchModal.vue
    - add back  basic filter functionality, creates a map of each file that matches conditions and emits its to parent component

--- client/src/components/SearchModal.vue
+++ client/src/components/SearchModal.vue
@@ -2,23 +2,50 @@
   <CenterModal :show="show" @close="close">
     <h2>Search Media</h2>
     <!-- TODO - custom error messages and validation -->
-    <v-form @submit.prevent>
-      <v-text-field name="title" label="Title" id="title" clearable
+    <v-form @submit.prevent="submit">
+      <v-text-field
+        v-model="title"
+        name="title"
+        label="Title"
+        id="title" clearable
       ></v-text-field>
-      <v-text-field name="creator" label="Creator" id="creator" clearable
+      <v-text-field
+        v-model="creator"
+        name="creator"
+        label="Creator"
+        id="creator"
+        clearable
       ></v-text-field>
-      <v-text-field name="description" label="Description" id="description" clearable
+      <v-text-field
+        v-model="description"
+        name="description"
+        label="Description"
+        id="description"
+        clearable
       ></v-text-field>
       <v-select
+        v-model="tags"
         :items="tagList"
         label="Tags"
         multiple
         chips
         :menu-props="{ zIndex: 9999 }"
       ></v-select>
-      <v-text-field name="dateFrom" label="From" id="dateFrom" type="date" clearable
+      <v-text-field
+        v-model="dateFrom"
+        name="dateFrom"
+        label="From"
+        id="dateFrom"
+        type="date"
+        clearable
       ></v-text-field>
-      <v-text-field name="dateTo" label="To" id="dateTo" type="date" clearable
+      <v-text-field
+        v-model="dateTo"
+        name="dateTo"
+        label="To"
+        id="dateTo"
+        type="date"
+        clearable
       ></v-text-field>
       <v-btn type="submit" name="submit" value="Submit">Submit</v-btn>
     </v-form>
@@ -43,10 +70,59 @@ export default {
       default: () => new Map(),
     },
   },
+  data () {
+    return {
+      title: '',
+      creator: '',
+      description: '',
+      tags: [],
+      dateFrom: '',
+      dateTo: '',
+    };
+  },
   methods: {
     close () {
       this.$emit('close');
     },
+    /**
+     * @async
+     * @param {Event} e
+     */
+    async submit (e) {
+      const filteredFiles = new Map();
+      for (const file of this.files.values()) {
+        if (this.title && !file.metadata.title.includes(this.title)) {
+          continue;
+        }
+        if (this.creator && !file.metadata.creator.includes(this.creator)) {
+          continue;
+        }
+        if (this.description && !file.metadata.description.includes(this.description)) {
+          continue;
+        }
+        if (this.dateFrom && file.metadata.date < this.dateFrom) {
+          continue;
+        }
+        if (this.dateTo && file.metadata.date > this.dateTo) {
+          continue;
+        }
+        if (this.tags.length) {
+          let found = false;
+          for (const tag of this.tags) {
+            if (file.metadata.tags.includes(tag)) {
+              found = true;
+              break;
+            }
+          }
+          if (!found) {
+            continue;
+          }
+        }
+        filteredFiles.set(file._id, file);
+      }
+      e.target.reset();
+      this.$emit('filteredFiles', filteredFiles);
+    }
   },
   computed: {
     tagList () {
