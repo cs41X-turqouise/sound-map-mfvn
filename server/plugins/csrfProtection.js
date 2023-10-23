73ad9176da20ebd0c040b9cf8102cef231fc4b64
Benjamin Johnson <bjohn108@odu.edu>
2023-10-23 17:53:54 -0400

    Added handler route logic

--- server/plugins/csrfProtection.js
+++ server/plugins/csrfProtection.js
@@ -4,6 +4,7 @@ const fp = require('fastify-plugin');
 module.exports = fp(async function (fastify, options) {
 
   fastify.register(require('@fastify/csrf-protection'), {
-      sessionPlugin: '@fastify/session'
+      sessionPlugin: '@fastify/session',
+      getToken: (req) => { return req.headers['csrf-token']}
   });
 }),  { name: 'csrf-protection' };
--- server/routes/csrfProtection/index.js
+++ server/routes/csrfProtection/index.js
@@ -18,8 +18,37 @@ module.exports = async function (fastify, options) {
         path: '/',
         onRequest: fastify.csrfProtection,
         handler: async (req, reply) => {
-            return req.body
+            const token = await reply.generateCsrf()
+            reply.type('text/html')
+            return `
+            <html>
+              <script type='text/javascript'>
+                async function test(event) {
+                  event.preventDefault()
+                  const rawResponse = await fetch('/', {
+                    method: 'POST',
+                    headers: {
+                      'Accept': 'application/json',
+                      'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ _csrf: '${token}'})
+                  });
+                  const content = await rawResponse.json();
+                  
+                  alert(JSON.stringify(content));
+                }
+              </script>
+              <body>
+                <form action="/" method='POST' id='form' onsubmit={test(event)}>
+                  <input type='submit' value='submit' />
+                </form>
+              </body>
+            </html>
+          
+          `
         }
     })
+
+    fastify.log.info("CSRF Protection Route registered")
 
 }
