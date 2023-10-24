128a9761706bf96607304caa783a8df6c48e8f28
tr29038 <thmire2016@gmail.com>
2023-10-24 17:20:18 -0400

    Fix formatting

--- server/plugins/csrfProtection.js
+++ server/plugins/csrfProtection.js
@@ -2,9 +2,8 @@
 const fp = require('fastify-plugin');
 
 module.exports = fp(async function (fastify, options) {
-
   fastify.register(require('@fastify/csrf-protection'), {
-      sessionPlugin: '@fastify/session',
-      getToken: (req) => { return req.headers['csrf-token']}
+    sessionPlugin: '@fastify/session',
+    getToken: (req) => { return req.headers['csrf-token']}
   });
-}),  { name: 'csrf-protection' };
+}), { name: 'csrf-protection' };
