6e85d0b394b239e2d305f765762e7e39f5e015f7
Cwoody240 <cwood012@odu.edu>
2023-10-29 21:26:18 -0400

    APIs for upload and auth routes are now available may need more properties and response codes if needed

--- server/routes/uploads/index.js
+++ server/routes/uploads/index.js
@@ -17,30 +17,47 @@ module.exports = async function (fastify, options) {
   /**
    * User upload a file
    */
-  fastify.post('/single',
-    {
-      preHandler: [
-        function (request, reply, done) {
-          if (!request.session.user) {
-            done(new Error('User not logged in'));
-          } else {
-            done();
-          }
+fastify.post('/single',
+  {
+    preHandler: [
+      function (request, reply, done) {
+        if (!request.session.user) {
+          done(new Error('User not logged in'));
+        } else {
+          done();
+        }
+      },
+      fastify.upload.fields([
+        { name: 'sound', maxCount: 1 },
+        { name: 'images', maxCount: 12 },
+      ]),
+      function (request, reply, done) {
+        if (!request.files) {
+          done(new Error('File is required'));
+        } else {
+          done();
+        }
+      },
+    ],
+    schema: {
+      tags: ['uploads'],
+      body: {
+        type: 'object',
+        properties: {
+          // Define properties for the request body here (if applicable)
         },
-        fastify.upload.fields([
-          { name: 'sound', maxCount: 1 },
-          { name: 'images', maxCount: 12 },
-        ]),
-        function (request, reply, done) {
-          if (!request.files) {
-            done(new Error('File is required'));
-          } else {
-            done();
-          }
+      },
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            _id: { type: 'string', description: 'MongoDB ObjectId' },
+          },
         },
-      ]
+        // Add more response codes if needed
+      },
     },
-    async function (request, reply) {
+    async handler(request, reply) {
       fastify.log.info(request.files);
       const userId = fastify.toObjectId(request.session.user._id);
       const sound = request.files.sound[0];
@@ -66,212 +83,338 @@ module.exports = async function (fastify, options) {
       await upload.save();
       return sound;
     }
-  );
+  }
+);
   
   /**
    * Does not work
    * Allows users to upload an array of files
    * @todo We should be able to upload multiple sound files and corresponding images if any
    */
-  fastify.post('/bulk',
-    {
-      preHandler: fastify.upload.array('files', 12)
+  fastify.post('/bulk', {
+    preHandler: fastify.upload.array('files', 12),
+    schema: {
+      tags: ['uploads'],
+        body: {
+            type: 'object',
+            properties: {
+                user: { type: 'string', description: 'MongoDB ObjectId of the user' },
+            },
+            required: ['user'],
+        },
+        response: {
+            200: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        // Define properties for each uploaded file object here (if applicable)
+                        _id: { type: 'string', description: 'MongoDB ObjectId' },
+                        // Add more properties as needed
+                    },
+                },
+            },
+            // Add more response codes if needed
+        },
     },
-    /**
-     * Handles the upload of multiple files.
-     * @param {Request} request - The request object.
-     * @param {Reply} reply - The reply object.
-     * @returns {Array} An array of uploaded file objects.
-     */
-    async function (request, reply) {
-      // Extract the user ID from the request body
-      const userId = fastify.toObjectId(request.body.user);
+    async handler(request, reply) {
+        // Extract the user ID from the request body
+        const userId = fastify.toObjectId(request.body.user);
 
-      // Process file uploads concurrently using Promise.all and map by using Promise.all
-      // in combination with map, the code is able to process multiple file uploads simultaneously
-      const uploads = await Promise.all(request.files.map(async (file) => {
-        // Create a new Upload object with file details and user ID
-        const sound = request.files.sound[0];
-        const upload = new Sound({
-          ...request.file,
-          user: userId,
-          _id: fastify.toObjectId(sound.id),
-        });
-        // Save the Upload object to the database and return the result
-        return await upload.save();
-      }));
+        // Process file uploads concurrently using Promise.all and map by using Promise.all
+        // in combination with map, the code is able to process multiple file uploads simultaneously
+        const uploads = await Promise.all(request.files.map(async (file) => {
+            // Create a new Upload object with file details and user ID
+            const sound = request.files.sound[0];
+            const upload = new Sound({
+                ...request.file,
+                user: userId,
+                _id: fastify.toObjectId(sound.id),
+            });
+            // Save the Upload object to the database and return the result
+            return await upload.save();
+        }));
 
-      // Return an array of uploaded file objects
-      return uploads;
+        // Return an array of uploaded file objects
+        return uploads;
     }
-  );
+});
 
   /**
    * Get all uploads
    * 10/13/23 - Works but it's slow
    * @todo Speed this up
    */
-  fastify.get('/', async function (request, reply) {
-    const data = [];
-    const uploads = await Sound.find({});
-    for (const upload of uploads) {
-      const { file, fileStream } = await upload.getFileStream(fastify);
-      const buffer = await new Promise((resolve, reject) => {
-        const chunks = [];
-        fileStream.on('data', (chunk) => chunks.push(chunk));
-        fileStream.on('error', reject);
-        fileStream.on('end', () => resolve(Buffer.concat(chunks)));
-      });
-      data.push({
-        file,
-        buffer,
-      });
-    }
-    return reply.send(data);
-  });
-  /**
-   * Get a specific upload
-   */
-  fastify.get('/:id', async function (request, reply) {
-    const _id = fastify.toObjectId(request.params.id);
-    const fileDoc = await Sound.findById(_id).exec();
-    const { file, fileStream } = await fileDoc.getFileStream(fastify);
-    reply.header('Content-Type', file.contentType);
-    return reply.send(fileStream);
-  });
+  fastify.get('/', {
+    schema: {
+        tags: ['uploads'],
+        response: {
+            200: {
+                type: 'array',
+                items: {
+                    type: 'object',
+                    properties: {
+                        file: { type: 'object' }, // Define properties for 'file' object here
+                        buffer: { type: 'string', format: 'binary' }, // Assuming buffer is returned as binary data
+                        // Add more properties as needed
+                    },
+                },
+            },
+            // Add more response codes if needed
+        },
+    },
+    async handler(request, reply) {
+        const data = [];
+        const uploads = await Sound.find({});
+        for (const upload of uploads) {
+            const { file, fileStream } = await upload.getFileStream(fastify);
+            const buffer = await new Promise((resolve, reject) => {
+                const chunks = [];
+                fileStream.on('data', (chunk) => chunks.push(chunk));
+                fileStream.on('error', reject);
+                fileStream.on('end', () => resolve(Buffer.concat(chunks)));
+            });
+            data.push({
+                file,
+                buffer,
+            });
+        }
+        return reply.send(data);
+    },
+});
 
   /**
    * Get all the file data from the sounds bucket
    */
-  fastify.get('/filedata/all', async function (request, reply) {
-    const data = [];
-    const uploads = await Sound.find({});
-    for (const upload of uploads) {
-      const file = await upload.getFile(fastify);
-      data.push(file);
-    }
-    return reply.send(data);
-  });
+  fastify.get('/filedata/all', {
+    schema: {
+        tags: ['uploads'],
+        response: {
+            200: {
+                type: 'array',
+                items: {
+                    type: 'object', // Define properties for each item in the array here
+                    // Add more properties as needed
+                },
+            },
+            // Add more response codes if needed
+        },
+    },
+    async handler(request, reply) {
+        const data = [];
+        const uploads = await Sound.find({});
+        for (const upload of uploads) {
+            const file = await upload.getFile(fastify);
+            data.push(file);
+        }
+        return reply.send(data);
+    },
+});
 
   /**
    * Find user who uploaded a file
    */
-  fastify.get('/:fileId/:userId', async function (request, reply) {
-    const user = await Sound.findById(request.params.fileId)
-      .populate('users')
-      .findById(request.params.userId);
-    return user;
-  });
-
+  fastify.get('/:fileId/:userId', {
+    schema: {
+        tags: ['uploads'],
+        params: {
+            type: 'object',
+            properties: {
+                fileId: { type: 'string', description: 'MongoDB ObjectId of the file' },
+                userId: { type: 'string', description: 'MongoDB ObjectId of the user' },
+            },
+        },
+        response: {
+            200: {
+                type: 'object', // Define properties for the response object
+                // Add more properties as needed
+            },
+        },
+    },
+    async handler(request, reply) {
+        const user = await Sound.findById(request.params.fileId)
+            .populate('users')
+            .findById(request.params.userId);
+        return user;
+    },
+});
   /**
    * Delete a sound file - should be Admin only or limited to the user themselves
    */
-  fastify.delete('/sound/:id', async function (request, reply) {
-    try {
-      const _id = fastify.toObjectId(request.params.id);
-      await fastify.gridfsSounds.delete(_id);
-      const file = await Sound.findByIdAndDelete(_id);
-      if (file.images) {
-        // delete all associated images
-        for (const image of file.images) {
-          await fastify.gridfsImages.delete(_id);
-          await Image.findByIdAndDelete(image);
-        }
-      }
-      const users = await User.find({ uploads: file._id });
+  fastify.delete('/sound/:id', {
+    schema: {
+        tags: ['uploads'],
+        params: {
+            type: 'object',
+            properties: {
+                id: { type: 'string', description: 'MongoDB ObjectId of the sound file' },
+            },
+        },
+        response: {
+            200: {
+                type: 'object', // Define properties for the response object
+                // Add more properties as needed
+            },
+        },
+    },
+    async handler(request, reply) {
+        try {
+            const _id = fastify.toObjectId(request.params.id);
+            await fastify.gridfsSounds.delete(_id);
+            const file = await Sound.findByIdAndDelete(_id);
+            if (file.images) {
+                // delete all associated images
+                for (const image of file.images) {
+                    await fastify.gridfsImages.delete(image);
+                    await Image.findByIdAndDelete(image);
+                }
+            }
+            const users = await User.find({ uploads: file._id });
 
-      // Remove the file ID from every user's uploads array
-      for (const user of users) {
-        user.uploads.pull(file._id);
-        await user.save();
-      }
-      return file;
-    } catch (err) {
-      fastify.log.error(err);
-    }
-  });
+            // Remove the file ID from every user's uploads array
+            for (const user of users) {
+                user.uploads.pull(file._id);
+                await user.save();
+            }
+            return file;
+        } catch (err) {
+            fastify.log.error(err);
+            throw new Error('Internal Server Error');
+        }
+    },
+});
 
   /**
    * Delete a image file - should be Admin only or limited to the user themselves
    */
-  fastify.delete('/image/:id', async function (request, reply) {
-    try {
-      const _id = fastify.toObjectId(request.params.id);
-      await fastify.gridfsImages.delete(_id);
-      const file = await Image.findByIdAndDelete(_id);
-      const sound = await Sound.findById(file.soundFile);
-      sound.images.pull(file._id);
-      await sound.save();
-      return file;
-    } catch (err) {
-      fastify.log.error(err);
-    }
-  });
-
+  fastify.delete('/image/:id', {
+    schema: {
+        tags: ['uploads'],
+        params: {
+            type: 'object',
+            properties: {
+                id: { type: 'string', description: 'MongoDB ObjectId of the image file' },
+            },
+        },
+        response: {
+            200: {
+                type: 'object', // Define properties for the response object
+                // Add more properties as needed
+            },
+        },
+    },
+    async handler(request, reply) {
+        try {
+            const _id = fastify.toObjectId(request.params.id);
+            await fastify.gridfsImages.delete(_id);
+            const file = await Image.findByIdAndDelete(_id);
+            const sound = await Sound.findById(file.soundFile);
+            sound.images.pull(file._id);
+            await sound.save();
+            return file;
+        } catch (err) {
+            fastify.log.error(err);
+            throw new Error('Internal Server Error');
+        }
+    },
+});
   /**
    * Rename a file - should be Admin only or limited to the user themselves
    */
-  fastify.patch('/filename/:id',
-    {
-      schema: {
-        body: {
-          type: 'object',
-          required: ['newFileName'],
-          properties: {
-            newFileName: { type: 'string' },
-          },
+  fastify.patch('/filename/:id', {
+    schema: {
+        tags: ['uploads'],
+        params: {
+            type: 'object',
+            properties: {
+                id: { type: 'string', description: 'MongoDB ObjectId of the file' },
+            },
         },
-      },
+        body: {
+            type: 'object',
+            required: ['newFileName'],
+            properties: {
+                newFileName: { type: 'string', description: 'New file name' },
+            },
+        },
+        response: {
+            200: {
+                type: 'object', // Define properties for the response object
+                // Add more properties as needed
+            },
+        },
     },
-    async function (request, reply) {
-      const _id = fastify.toObjectId(request.params.id);
-      const newFileName = request.body.newFileName;
+    async handler(request, reply) {
+        try {
+            const _id = fastify.toObjectId(request.params.id);
+            const newFileName = request.body.newFileName;
 
-      // Renames the file to new file name
-      const file = await fastify.gridfsSounds.rename(_id, newFileName);
-      // const file = await Upload.findByIdAndUpdate(_id, {
-      //   filename: newFileName
-      // }, { new: true });
-      return file;
-    }
-  );
+            // Renames the file to new file name
+            const file = await fastify.gridfsSounds.rename(_id, newFileName);
+            return file;
+        } catch (err) {
+            fastify.log.error(err);
+            throw new Error('Internal Server Error');
+        }
+    },
+});
 
   /**
    * Update metadata of a file - should be Admin only or limited to the user themselves
    */
-  fastify.patch('/metadata/:id',
-    {
-      schema: {
+  fastify.patch('/metadata/:id', {
+    schema: {
+        tags: ['uploads'],
+        params: {
+            type: 'object',
+            properties: {
+                id: { type: 'string', description: 'MongoDB ObjectId of the file' },
+            },
+        },
         body: {
-          type: 'object',
-          properties: {
-            title: { type: 'string' },
-            description: { type: 'string' },
-            tags: { type: 'array' },
-            latitude: { type: 'number' },
-            longitude: { type: 'number' },
-          },
+            type: 'object',
+            properties: {
+                title: { type: 'string', description: 'New title for the file' },
+                description: { type: 'string', description: 'New description for the file' },
+                tags: { type: 'array', items: { type: 'string' }, description: 'Array of tags' },
+                latitude: { type: 'number', description: 'New latitude value' },
+                longitude: { type: 'number', description: 'New longitude value' },
+            },
         },
-      },
+        response: {
+            200: {
+                type: 'object', // Define properties for the response object
+                // Add more properties as needed
+            },
+        },
     },
-    async function (request, reply) {
-      const _id = fastify.toObjectId(request.params.id);
-      if (!Object.keys(request.body).length) {
-        throw new Error('No metadata provided');
-      }
-      const oldFile = await fastify.mongoose.connection.db.collection('sounds.files').findOne({ _id: _id });
-      const metadata = { ...oldFile.metadata, ...request.body };
-      oldFile.metadata = metadata;
-      const file = await fastify.mongoose.connection.db.collection('sounds.files').findOneAndUpdate(
-        { _id },
-        {
-          $set: {
-            metadata: metadata
-          }
-        },
-        { returnOriginal: false }
-      );
-      return file;
-    }
-  );
+    async handler(request, reply) {
+        try {
+            const _id = fastify.toObjectId(request.params.id);
+
+            if (!Object.keys(request.body).length) {
+                throw new Error('No metadata provided');
+            }
+
+            const oldFile = await fastify.mongoose.connection.db.collection('sounds.files').findOne({ _id: _id });
+            const metadata = { ...oldFile.metadata, ...request.body };
+            oldFile.metadata = metadata;
+
+            const file = await fastify.mongoose.connection.db.collection('sounds.files').findOneAndUpdate(
+                { _id },
+                {
+                    $set: {
+                        metadata: metadata,
+                    },
+                },
+                { returnOriginal: false }
+            );
+
+            return file;
+        } catch (err) {
+            fastify.log.error(err);
+            throw new Error('Internal Server Error');
+        }
+    },
+});
 };
