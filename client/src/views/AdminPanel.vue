82e96647b6fd75c3c0e784cc4ec35ac3307d2e6b
Prestin Bell <prestin97@gmail.com>
2023-10-30 20:55:32 -0400

    moved the file to .vue and reset adminPanel

---
+++ client/src/views/AdminPanel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class = 'AdminPage'>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminPage',
+  data () {
+    return {
+
+    };
+  }
+
+};
+</script>
+
+<style>
+
+</style>
