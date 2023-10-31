483baab33279aabdaa6829b1c4fd8fa7d3a3d3bb
MeagherPatrick <pmeag001@odu.edu>
2023-10-30 23:55:11 -0400

    Needs one less require

--- server/scripts/clean-db.js
+++ server/scripts/clean-db.js
@@ -1,6 +1,10 @@
-require('dotenv').config({ path: './local.env' });
-const mongoose = require('mongoose');
+ require('dotenv').config({ path: './local.env' });
 
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import User from '../models/User.js';
+import Sound from '../models/Sound.js';
+import Image from '../models/Image.js';
 /**
  * Cleans the MongoDB database.
  * @async
@@ -9,11 +13,8 @@ async function cleanDatabase () {
   try {
     await mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Connected to MongoDB');
-    const User = require('../models/User');
-    const Sound = require('../models/Sound');
-    const Image = require('../models/Image');
-
-    const soundBucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
+    
+      const soundBucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
       bucketName: 'sounds'
     });
     const imageBucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db, {
