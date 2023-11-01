2efc31e0b98ca3211eb22f15810f3a056236fc5b
Prestin Bell <prestin97@gmail.com>
2023-10-31 22:19:01 -0400

    implemented the template in HTML for adminPanel

--- client/src/views/AdminPanel.vue
+++ client/src/views/AdminPanel.vue
@@ -1,15 +1,41 @@
 <template>
   <div class = 'AdminPage'>
-
+    <h1>User List</h1>
+    <table class='userTable'>
+      <thead>
+        <tr>
+          <th>User ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Uploads</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, index) in users" :key="index">
+          <td>{{ user._id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>
+            <span v-for="(upload, uploadIndex) in user.uploads" :key="uploadIndex">
+              {{ upload.fileName }}{{ uploadIndex < user.uploads.length - 1 ? ', ' : '' }}
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AdminPage',
+  /*
+  // componets: {
+  },
+  */
   data () {
     return {
-
+      users: [],
     };
   }
 
