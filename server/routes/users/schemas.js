c9797439f643fa536e7f0f021a396508d68ef175
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-11-03 19:47:06 -0400

    Update schemas.js
    - convert to esm

--- server/routes/users/schemas.js
+++ server/routes/users/schemas.js
@@ -2,7 +2,7 @@
  * Schemas for user routes
  */
 
-const userSchema = {
+export const userSchema = {
   type: 'object',
   properties: {
     _id: { type: 'string', description: 'MongoDB ObjectId' },
@@ -13,8 +13,4 @@ const userSchema = {
     uploads: { type: 'array', items: { type: 'string', description: 'MongoDB ObjectId' } },
     bookmarks: { type: 'array', items: { type: 'string', description: 'MongoDB ObjectId' } },
   }
-};
-
-module.exports = {
-  userSchema
 };
