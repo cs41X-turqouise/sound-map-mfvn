0604b44c402ad556daf1149a14f6adb5b76541c3
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-11-04 17:56:45 -0400

    Update to ESM

--- server/plugins/csrfProtection.js
+++ server/plugins/csrfProtection.js
@@ -1,16 +1,15 @@
-'use strict';
-const fp = require('fastify-plugin');
+import fp from 'fastify-plugin';
 
 /**
  * This plugins enables usage of csrf protection.
  *
  * @see https://github.com/fastify/csrf-protection
  */
-module.exports = fp(async function (fastify, options) {
-  fastify.register(require('@fastify/csrf-protection'), {
+export default fp(async function (fastify, options) {
+  fastify.register(import('@fastify/csrf-protection'), {
     sessionPlugin: '@fastify/session',
     getToken: (req) => {
       return req.headers['csrf-token'];
     }
   });
-}), { name: 'csrf-protection' };
+}, { name: 'csrf-protection' });
--- server/plugins/helmet.js
+++ server/plugins/helmet.js
@@ -1,13 +1,11 @@
-'use strict';
-
-const fp = require('fastify-plugin');
+import fp from 'fastify-plugin';
 
 /**
  * Important security headers for Fastify.
  * @see https://github.com/fastify/fastify-helmet
  */
-module.exports = fp(async function (fastify, opts) {
-  fastify.register(require('@fastify/helmet'), {
+export default fp(async function (fastify, opts) {
+  fastify.register(import('@fastify/helmet'), {
     global: true,
   });
 });
