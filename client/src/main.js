f8368c06f5788134cf1b253c76caa009229dae87
Kori Fogle <kfogl001@odu.edu>
2023-11-05 23:06:34 -0500

    Moved vuetify to its own file in a new plugins directory

--- client/src/main.js
+++ client/src/main.js
@@ -3,28 +3,7 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
-import 'vuetify/styles';
-import '@mdi/font/css/materialdesignicons.css';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-import { aliases, mdi } from 'vuetify/iconsets/mdi';
-
-const vuetify = createVuetify({
-  components,
-  directives,
-  icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi,
-    }
-  },
-  theme: {
-    defaultTheme: 'light',
-  },
-});
+import { vuetify } from './plugins/vuetify';
 
 createApp(App)
   .use(store)
