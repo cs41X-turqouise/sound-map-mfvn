8174f183394d06dda90a14ba68551d72b724d1dd
Prestin Bell <prestin97@gmail.com>
2023-11-09 21:23:59 -0500

    template for page

--- client/src/views/AboutPage.vue
+++ client/src/views/AboutPage.vue
@@ -1,1 +1,20 @@
+<template>
+  <div class="about-us">
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <h1 class="page-title">About Us</h1>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-card class="section-card" v-for="(section, index) in sections" :key="index">
+            <v-card-title class="section-title">{{ section.title }}</v-card-title>
+            <v-card-text class="section-content">{{ section.content }}</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
 
