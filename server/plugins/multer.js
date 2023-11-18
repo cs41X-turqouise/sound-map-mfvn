2dc4ff922beef78c8da1a819ded0154673b08959
MeagherPatrick <pmeag001@odu.edu>
2023-11-17 19:57:33 -0500

    eslint fix

--- server/plugins/multer.js
+++ server/plugins/multer.js
@@ -9,8 +9,8 @@ export default fp(async function (fastify, options) {
     url: fastify.config.MONGODB_URL,
     // db: fastify.mongoose.connection.db,
     /**
-     * @param {Request} req     
-     * @param {import("@thebguy/multer-gridfs-storage").GridFile} file  
+     * @param {Request} req
+     * @param {import("@thebguy/multer-gridfs-storage").GridFile} file
      */
     file: (req, file) => {
       return new Promise((resolve, reject) => {
