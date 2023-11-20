2852a9aec202a3d24af14432a1499e64f9276327
theBGuy <60308670+theBGuy@users.noreply.github.com>
2023-11-20 01:01:09 -0500

    Update User.js
    - Create superadmin role

--- server/models/User.js
+++ server/models/User.js
@@ -49,7 +49,7 @@ const userSchema = new mongoose.Schema({
   }],
   role: {
     type: String,
-    enum: ['user', 'moderator', 'admin'],
+    enum: ['user', 'moderator', 'admin', 'superadmin'],
     default: 'user',
     lowercase: true,
   },
